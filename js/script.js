6f93c57fca8413ac3e09ed552967c90bd2faee98
Victor Vasquez <91144991+Victrex@users.noreply.github.com>
2021-12-22 17:34:38 -0600

    news

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let t0 = gsap.timeline({
+    scrollTrigger:{
+        trigger: '.descripcion',
+        start:"center bottom"
+    }
+})
+
+t0.from(".des_h2",{y:-100, opacity:0, duration:1.5})
+    .from(".desp_p",{y:-100, opacity:0, duration: 1}, "-=1")
+
+let t1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.dark',
+        start:"center bottom"
+    }
+});
+
+t1.from(".photo", {x:200, opacity:0, duration: 1.5 })
+    .from(".content",{y:300, opacity: 0, duration: 1}, "-=0.5")
+    .from(".skills",{y:300, opacity:0, duration:1}, "-=0.3")
+
+let t3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.made',
+        start: "center bottom"
+    }
+});
+
+t3.from(".md1",{rotationX:90, y:-40, z:10, opacity:0, duration: 0.5})
+t3.from(".md2",{rotationX:90, y:-40, z:10, opacity:0, duration: 0.5})
+t3.from(".md3",{rotationX:90, y:-40, z:10, opacity:0, duration: 0.5})
+t3.from(".md4",{rotationX:90, y:-40, z:10, opacity:0, duration: 0.5})
+t3.from(".md5",{rotationX:90, y:-40, z:10, opacity:0, duration: 0.5})
+t3.from(".md6",{rotationX:90, y:-40, z:10, opacity:0, duration: 0.5})
+
+
+let t4 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.made',
+        start: "center bottom"
+    }
+});
+
+
+t4.from("#post.ab1, #post.ab2",{rotationX:90, y:-40, z:10, opacity:0, duration: 0.5})
+t4.from("#post.ab3, #post.ab4, #post.ab5",{rotationX:90, y:-40, z:10, opacity:0, duration: 0.5})
+t4.from(".ab6",{x:-200, rotationY:-90,  opacity:0, duration: 1.5})
+t4.from(".ab7",{rotationX:90, y:-40, z:10, opacity:0, duration: 1.5}, "-=0.5")
+
